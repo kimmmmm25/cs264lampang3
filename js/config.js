87cb1fcb5d3486b757e92e79677ac45453268b1c
Tanaphat Tonai <tanaphat.ton@dome.tu.ac.th>
2020-11-28 17:58:51 +0700

    264

---
+++ js/config.js
@@ -0,0 +1,1 @@
+window.config={"logo":"","background":"#000000","shell":"web","showControlPanel":false,"iOSGuide":true,"alwaysShowLinks":false,"hoverShowLinks":false,"hoverShowRemarks":true,"showComments":true,"showQRCode":false,"hintLinks":false,"scrollRemarks":false,"isShowAllPageNode":false,"isShowPreviewTools":false,"showRemarkFlag":false}
